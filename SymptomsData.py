256915fea19390db3564424bd957af25796ace8c
hadi kotaich <hadyko.hk@gmail.com>
2020-04-02 23:36:10 +0300

    fixed some things in the random generator

--- SymptomsData.py
+++ SymptomsData.py
@@ -77,9 +77,7 @@ class SymptomsData:
 
                                       'direct contact'
                                       ]
-        synonyms['age'] = ['عمر',
-                        'age'
-                        ]
+        synonyms['age'] = [ ]
 
         for symptom in synonyms.keys():
             symptoms.append(symptom)
